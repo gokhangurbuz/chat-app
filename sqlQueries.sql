27702b657001e8979b6d5aabd316bc86618f21b2
gokhangurbuz <gokhangurbuz24@gmail.com>
2016-06-27 21:34:48 +0300

    postgreSql script added.

---
+++ sqlQueries.sql
@@ -0,0 +1,25 @@
+﻿-- Table: public.chatlogs
+
+-- DROP TABLE public.chatlogs;
+
+CREATE TABLE public.chatlogs
+(
+  id serial primary key,
+  sendername character varying(50),
+  message text,
+  createdate timestamp without time zone
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.chatlogs
+  OWNER TO ouningwemyxhot;
+
+-- Index: public.chatlogs_create_date_index
+
+-- DROP INDEX public.chatlogs_create_date_index;
+
+CREATE INDEX chatlogs_create_date_index
+  ON public.chatlogs
+  USING btree
+  (createdate);
